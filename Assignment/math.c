1694806d9de386aa7fb8c6b96f32f546c57ac550
Kevin Beenders <35497869+Kevin-Beenders@users.noreply.github.com>
2018-09-10 15:53:16 -0400

    Update math.c

--- Assignment/math.c
+++ Assignment/math.c
@@ -1,4 +1,33 @@
 
-math(3,2,+);
-
-return math;
+int main(void)
+{
+    int math(int num1, int num2, char Operator);
+    switch(Operator) {
+      case '+' : return (num1+num2); //Addition
+	break;
+      case '-' : return (num1-num2); //Subtraction
+	break;
+      case '*' : return (num1*num2); //Multiplication
+      	break;
+      case '/' : return (num1/num2); //Division
+      	break;
+      case '%' : return (num1%num2); //Remainder
+     	break;
+      case '<' : return (num1<num2); //Shift Left
+      	break;
+      case '>' : return (num1>num2); //Shift Right
+      	break;
+      case '&' : return (num1&num2); //Bitwise AND
+      	break;
+      case '|' : return (num1|num2); //Bitwise OR
+      	break;
+      case '^' : return (num1^num2); //Bitwise XOR
+      	break;
+      case '~' : return (~num1);     //Inverse
+	break;
+      default:
+	printf("Invalid Entry"); // no proper characters selected
+	break;
+    }	
+	return 0;
+}
